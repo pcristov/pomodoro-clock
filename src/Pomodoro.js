b844326625cf3c7b84086e6924c459a0c271e686
Pedro Cristovão <pcristov@gmail.com>
2020-08-31 23:34:05 +0800

    Implement functions reset, increment and decrement session and break Update styles

--- src/Pomodoro.js
+++ src/Pomodoro.js
@@ -7,10 +7,15 @@ export default class Pomodoro extends Component {
 	    super(props)
 	    
 	    this.state = {
-
+			breakLength: 50,
+			sessionLength: 25
 	    }
 	    
 	    this.addToExpression = this.addToExpression.bind(this);
+	    this.breakIncrement = this.breakIncrement.bind(this);
+	    this.breakDecrement = this.breakDecrement.bind(this);
+	    this.sessionIncrement = this.sessionIncrement.bind(this);
+	    this.sessionDecrement = this.sessionDecrement.bind(this);
 	    this.reset = this.reset.bind(this);
 	}
 	
@@ -18,9 +23,34 @@ export default class Pomodoro extends Component {
 		let input = e.target.innerHTML;
 	}
 	
+	breakIncrement() {
+		this.setState(prevState => ({
+			breakLength: (prevState.breakLength < 60) ? prevState.breakLength + 1 : 60
+		}))
+	}
+	
+	breakDecrement() {
+		this.setState(prevState => ({
+			breakLength: (prevState.breakLength > 1) ? prevState.breakLength - 1 : 1
+	    }))
+	}
+	
+	sessionIncrement() {
+		this.setState(prevState => ({
+			sessionLength: (prevState.sessionLength < 60) ? prevState.sessionLength + 1 : 60
+		}))
+	}
+	
+	sessionDecrement() {
+		this.setState(prevState => ({
+			sessionLength: (prevState.sessionLength > 1) ? prevState.sessionLength - 1 : 1
+	    }))
+	}
+	
 	reset() {
 		this.setState({
-			
+			breakLength: 5,
+			sessionLength: 25
 	    });
 	}
 	
@@ -32,31 +62,23 @@ export default class Pomodoro extends Component {
 				<div className="break-label" id="break-label">Break</div>
 				<div className="session-label" id="session-label">Session</div>
 				
-				<div className="break-increment" id="break-increment" onClick={ this.reset }>
-					<i class="material-icons">add_circle</i>	
-				</div>
-				<div className="break-length" id="break-length">time</div>
-				<div className="break-decrement" id="break-decrement" onClick={ this.reset }>
-					<i class="material-icons">remove_circle</i>
-				</div>
+				<div className="break-decrement" id="break-decrement" onClick={ this.breakDecrement }><i className="material-icons">remove_circle</i></div>
+				<div className="break-length" id="break-length">{ this.state.breakLength }</div>
+				<div className="break-increment" id="break-increment" onClick={ this.breakIncrement }><i className="material-icons">add_circle</i></div>
 				
-				<div className="session-increment" id="session-increment" onClick={ this.reset }>
-					<i class="material-icons">add_circle</i>
-				</div>
+				<div className="session-decrement" id="session-decrement" onClick={ this.sessionDecrement }><i className="material-icons">remove_circle</i></div>
+				<div className="session-length" id="session-length">{ this.state.sessionLength }</div>
+				<div className="session-increment" id="session-increment" onClick={ this.sessionIncrement }><i className="material-icons">add_circle</i></div>
+				
 				<div className="timer-label" id="timer-label">Session</div>
 				<div className="time-left" id="time-left">-</div>
-				<div className="session-decrement" id="session-decrement" onClick={ this.reset }>
-					<i class="material-icons">remove_circle</i>
-				</div>
-				
-				<div className="session" id="session">session</div>
 				
 				<div className="start_stop" id="start_stop" onClick={ this.start }>
-					<i class="material-icons">play_arrow</i>
-					<i class="material-icons">pause</i>
+					<i className="material-icons">play_arrow</i>
+					<i className="material-icons">pause</i>
 				</div>
 				<div className="reset" id="reset" onClick={ this.reset }>
-					<i class="material-icons">refresh</i>
+					<i className="material-icons">refresh</i>
 				</div>
 			</div>
 	)}
