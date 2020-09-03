715ddd1db0d274f1096dace6e6dbf7e7977c3a7c
Pedro Cristovão <pcristov@gmail.com>
2020-09-03 18:31:17 +0800

    Implement logic for break/session cycle

--- src/Pomodoro.js
+++ src/Pomodoro.js
@@ -1,14 +1,21 @@
 import React, {Component} from 'react';
 import './Pomodoro.css';
 
+let interval;
+const type = ['Break', 'Session'];
+
 export default class Pomodoro extends Component {
 
 	constructor(props) {
 	    super(props)
 	    
 	    this.state = {
-			breakLength: 50,
-			sessionLength: 25
+			breakLength: 1,
+			sessionLength: 1,
+			minutes: 0,
+			seconds: 1,
+			pause: 1,
+			type: type[1]
 	    }
 	    
 	    this.addToExpression = this.addToExpression.bind(this);
@@ -16,6 +23,7 @@ export default class Pomodoro extends Component {
 	    this.breakDecrement = this.breakDecrement.bind(this);
 	    this.sessionIncrement = this.sessionIncrement.bind(this);
 	    this.sessionDecrement = this.sessionDecrement.bind(this);
+	    this.start = this.start.bind(this);
 	    this.reset = this.reset.bind(this);
 	}
 	
@@ -26,25 +34,88 @@ export default class Pomodoro extends Component {
 	breakIncrement() {
 		this.setState(prevState => ({
 			breakLength: (prevState.breakLength < 60) ? prevState.breakLength + 1 : 60
-		}))
+		}), () => {
+	        this.setState({
+	            minutes: this.state.breakLength,
+	            seconds: 0
+	        })
+	    })
 	}
 	
 	breakDecrement() {
 		this.setState(prevState => ({
 			breakLength: (prevState.breakLength > 1) ? prevState.breakLength - 1 : 1
-	    }))
+		}), () => {
+	        this.setState({
+	            minutes: this.state.breakLength,
+	            seconds: 0
+	        })
+	    })
 	}
 	
 	sessionIncrement() {
 		this.setState(prevState => ({
 			sessionLength: (prevState.sessionLength < 60) ? prevState.sessionLength + 1 : 60
-		}))
+		}), () => {
+	        this.setState({
+	            minutes: this.state.sessionLength,
+	            seconds: 0
+	        })
+	    })
 	}
 	
 	sessionDecrement() {
 		this.setState(prevState => ({
 			sessionLength: (prevState.sessionLength > 1) ? prevState.sessionLength - 1 : 1
-	    }))
+		}), () => {
+	        this.setState({
+	            minutes: this.state.sessionLength,
+	            seconds: 0
+	        })
+	    })
+	}
+	
+	start(isCycle = 1) {
+		this.setState({
+			pause: isCycle === 1 ? 0 : !this.state.pause
+		}, () => {
+			if(this.state.pause) {
+				clearInterval(interval)
+			}
+			else
+			{
+				interval = setInterval(() => {
+					const minutes = this.state.minutes;
+					const seconds = this.state.seconds;
+					
+				    if(seconds > 0) {
+				      this.setState({
+				        seconds: seconds - 1
+				      })
+				    }
+				    
+				    if(seconds === 0) {
+				      if(minutes === 0) {
+				        this.setState({
+					        type: this.state.type === type[0] ? type[1] : type[0],
+					        minutes: this.state.type === 'Session' ? this.state.sessionLength : this.state.breakLength,
+					        seconds: 0
+				        },
+				        	() => {
+					        	clearInterval(interval);
+					        	this.start(1);
+					    	}
+					    )
+				      } else {
+				        this.setState({
+				          minutes: minutes - 1,
+				          seconds: 59
+				        })
+				      }
+				    }
+				}, 1000)
+			}
+		})
 	}
 	
 	reset() {
@@ -70,8 +141,8 @@ export default class Pomodoro extends Component {
 				<div className="session-length" id="session-length">{ this.state.sessionLength }</div>
 				<div className="session-increment" id="session-increment" onClick={ this.sessionIncrement }><i className="material-icons">add_circle</i></div>
 				
-				<div className="timer-label" id="timer-label">Session</div>
-				<div className="time-left" id="time-left">-</div>
+				<div className="timer-label" id="timer-label">{ this.state.type }</div>
+				<div className="time-left" id="time-left">{ this.state.minutes }:{ this.state.seconds < 10 ? `0${ this.state.seconds }` : this.state.seconds }</div>
 				
 				<div className="start_stop" id="start_stop" onClick={ this.start }>
 					<i className="material-icons">play_arrow</i>
